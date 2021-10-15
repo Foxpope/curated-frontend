cb25664e8407a932aaf34f927b3cce1ec96607cb
tdj182 <tdj182@gmail.com>
2021-10-15 13:34:48 -0500

    Movie service added

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { ClientMessage } from 'src/app/models/client-messages';
+import { MovieService } from 'src/app/services/movie.service';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-movie-detail',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailComponent implements OnInit {
 
-  constructor() { }
+  title="Movie Details"
+  // movie: Movie;
+  // public movieId: number; 
+  movie = new Movie(0, '', 0, '', '', '', '', '', '', '', '', '');
+
+  public clientMessage: ClientMessage = new ClientMessage('Sorry no movie to display');
+
+  constructor(private movieService: MovieService, 
+    private route: ActivatedRoute,
+    private location: Location) { 
+    // this.movieId = id;
+  }
 
   ngOnInit(): void {
+    // this.route.paramMap
+    //   .switchMap((params: ParamMap) => this.movieService.findByMovieId(+params.get('id')))
+    //   .subscribe(data => this.movie = movie);
+  }
+
+  public findMovieById(id: number) {
+    this.movieService.findByMovieId(id).subscribe(data => this.movie = data);
   }
 
 }
--- src/app/models/movie.ts
+++ src/app/models/movie.ts
@@ -1,11 +1,30 @@
 export class Movie {
   id: number;
   title: string;
-  genreId: number;
+  year: number;
+  rated: string;
+  released: string;
+  director: string;
+  plot: string;
+  poster: string;
+  genre: string;
+  metacritic: string;
+  runtime: string;
+  actors: string;
 
-  constructor(id: number, title: string, genreId: number) {
+  constructor(id: number, title: string, year: number, rated: string, released: string,
+    director: string, plot: string, poster: string,   genre: string, metacritic: string, runtime: string, actors: string) {
     this.id = id;
     this.title = title;
-    this.genreId = genreId
+    this.year = year;
+    this.rated = rated;
+    this.released= released;
+    this.director = director;
+    this.plot = plot;
+    this.poster = poster;
+    this.genre = genre;
+    this.metacritic = metacritic;
+    this.runtime = runtime;
+    this.actors = actors;
   }
 }
