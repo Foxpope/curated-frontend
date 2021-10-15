bbbc7736248af5a81b086f7ff75ad57498b7d417
Jacen <jbwyke@yahoo.com>
2021-10-14 20:07:56 -0700

    added html for movie details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,10 @@ import { UsersComponent } from './components/users/users.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { MainComponent } from './components/main/main.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
 const routes: Routes = [
+  {path:'movie-detail',component:MovieDetailComponent},
   {path:'welcome',component:WelcomeComponent},
   {path:'registration', component:RegistrationComponent},
   {path:'login', component:LoginComponent},
