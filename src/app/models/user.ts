f23d4ad337a2539558e4ef7ba226ad166ffa0856
Kirk Hahn <kirkhahn11@yahoo.com>
2021-10-15 13:37:35 -0500

    added user search functionallity and cleaned up logs from authentication process

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,3 +1,5 @@
+import { Review } from './review';
+import { Follow } from './follow';
 export class User {
   id: number;
   username: string;
@@ -5,13 +7,20 @@ export class User {
   password: string;
   firstName: string;
   lastName: string;
+  reviews: Array<Review>;
+  followers: Array<Follow>;
+  following: Array<Follow>;
 
-  constructor(id: number, username: string, email:string, password: string, firstName: string, lastName: string) {
+
+  constructor(id: number, username: string, email: string, password: string, firstName: string, lastName: string, reviews: Array<Review>, followers: Array<Follow>, following: Array<Follow>) {
     this.id = id;
     this.username = username;
     this.email = email;
     this.password = password;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.reviews = reviews;
+    this.followers = followers;
+    this.following = following;
   }
 }
