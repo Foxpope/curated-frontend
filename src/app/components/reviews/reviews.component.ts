fd74a1c57f5aba9badba35d450ad7ccf756c2f74
tdj182 <tdj182@gmail.com>
2021-10-16 14:44:56 -0500

    following/unfollowing within reviews is possible

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,21 +1,88 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit  } from '@angular/core';
 import { Review } from 'src/app/models/review';
+import { FollowService } from 'src/app/services/follow.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
+import { Follow } from 'src/app/models/follow';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.css']
 })
-export class ReviewsComponent {
+export class ReviewsComponent implements OnInit {
 
   @Input() reviews : Review[] = [];
+  currentUserId = Number(JSON.parse(sessionStorage.getItem('userId')!));
+  public currentUser = new User(0, '', '', '', '', '', [], [], [])
+  username = sessionStorage.getItem("username");
 
-  constructor() { 
+  //follow ID will be used when unfollow is clicked
+  followObject: Follow = new Follow(0, this.currentUser, new User(0, '', '', '', '', '', [], [], []), new Date());
+  
+
+  constructor(private followService: FollowService, private userService: UserService) { 
+  }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  public getUser(): void {
+    const temp = sessionStorage.getItem('username');
+    let username = temp ? JSON.stringify(temp) : "";
+    username = username.substring(1, username.length - 1);
+    this.userService.findByUsername(username)
+      .subscribe(
+        user => {
+          this.currentUser.id = user.id;
+          this.currentUser.email = user.email;
+          this.currentUser.firstName = user.firstName;
+          this.currentUser.lastName = user.lastName;
+          this.currentUser.password = user.password;
+          this.currentUser.username = user.username;
+          this.currentUser.following = user.following;
+          this.currentUser.followers = user.followers
+          this.currentUser.reviews = user.reviews;
+        }
+      )
   }
 
   ngOnChanges() {
-    this.reviews.forEach((review) => {
-      console.log(review.review);
-    })
+    // this.reviews.forEach((review) => {
+    //   console.log(review.review);
+    // })
   }
+
+  isFollowing(userId: number): boolean {
+    for (const followObject of this.currentUser.following) {
+      if (followObject.following.id === userId) {
+        this.followObject = followObject;
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  public followUser(userToFollowId: number): void {
+    console.log(userToFollowId);
+    if (this.isFollowing(userToFollowId)) {
+      this.followService.removeFollow(this.followObject.followId)
+      .subscribe(data => {
+        // Call Service to remove the the appropriate follow from the session storage
+        this.currentUser.following = this.followService.removeFromStorage(
+          this.currentUser.following, this.followObject.followId);
+      });
+    } else {
+      console.log("follow")
+      this.followService.addFollow(
+        this.currentUser, 
+        new User(userToFollowId, '', '', '', '', '', [], [], []))
+        .subscribe(data => this.currentUser.following.push(data))
+    }
+  }
+
+
 }
+// user-homepage?username=username
