94da1b3f048736229420c323fbb94d53789c8604
Kirk Hahn <kirkhahn11@yahoo.com>
2021-10-16 23:26:26 -0500

    movie from 3rd party API can now be persisted to our DB and will show up on the movie-details page if you click on their search icon

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -13,18 +13,18 @@ import { Movie } from 'src/app/models/movie';
 export class MoviesComponent implements OnInit {
 
   moviesArray: any[] = [];
+  page: number = 1;
+  term: string = '';
+  numberOfPages: number = 0;
   movie = new Movie('', '', 0, '', '', '', '', '', '', '', '', '', []);
   private searchTerms = new Subject<string>();
 
   constructor(private movieService: MovieService, private route: ActivatedRoute, private router: Router) { }
 
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
-
   ngOnInit(): void {
+    this.page = 1;
     this.route.params.subscribe(routeParams => {
-      this.getMovies(routeParams.search);
+      this.getMovies(routeParams.search, this.page);
     });
   }
 
@@ -57,18 +57,67 @@ export class MoviesComponent implements OnInit {
       )
   }
 
-  public getMovies(term: string): void {
-    this.movieService.searchMoviesByApiGeneric(term)
+  public getMovies(term: string, page: number): void {
+    this.term = term;
+    this.movieService.searchMoviesByApiGeneric(term, page)
       .subscribe(
         data => {
           this.moviesArray = data['Search']
-          console.log(this.moviesArray)
+          this.numberOfPages = Math.floor(data['totalResults']/10)
+          console.log(this.numberOfPages)
         },
         error => console.error(error)
       )
   }
-  public movieSelect(id: number): void {
-    this.router.navigateByUrl('movie-detail/id')
+
+  async movieSelect(id: string): Promise<void> {
+    this.movieService.searchMoviesByApiSpecific(id)
+      .subscribe(
+        data => {
+          this.movie.id = data.imdbID;
+          this.movie.title = data.Title;
+          this.movie.year = data.Year;
+          this.movie.rated = data.Rated;
+          this.movie.released = data.Released;
+          this.movie.director = data.Director;
+          this.movie.plot = data.Plot;
+          this.movie.poster = data.Poster;
+          this.movie.genre = data.Genre;
+          this.movie.metacritic = data.Metascore;
+          this.movie.runtime = data.Runtime;
+          this.movie.actors = data.Actors;
+          this.movie.reviews = [];
+          console.log(this.movie)
+          this.movieService.addMovie(this.movie)
+            .subscribe(
+              data => this.router.navigateByUrl(`movie-detail/${id}`),
+              error => console.error(error)
+            )
+        },
+        error => console.error(error)
+      )
   }
 
+
+  public nextPage(): void {
+    this.page++;
+    this.movieService.searchMoviesByApiGeneric(this.term, this.page)
+      .subscribe(
+        data => {
+          this.moviesArray = data['Search']
+        },
+        error => console.error(error)
+      )
+  }
+
+  public previousPage(): void {
+    this.page--;
+    this.movieService.searchMoviesByApiGeneric(this.term, this.page)
+      .subscribe(
+        data => {
+          this.moviesArray = data['Search']
+        },
+        error => console.error(error)
+      )
+  }
 }
